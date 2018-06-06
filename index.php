4bac957afba550160576f8f10185b2fdc0da64b0
DaniiKevin <40003873+DaniiKevin@users.noreply.github.com>
2018-06-06 22:07:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,255 @@
+﻿<?php
+	session_start();
+	
+	if(isset($_SESSION['username']))
+	{
+		echo $_SESSION['username'];
+	}
+	else
+	{
+		echo " ";
+	}	
+?>
+
+<html>
+    <head>
+        <title>Free Music Style ♀♂</title>
+        <meta charset="utf-8" />        
+		<link rel="shortcut icon" href="rec/musicc.ico" type="image/x-icon" />
+		<link type="text/css" rel="stylesheet" href="css/index.css"/>
+	
+		<!--Cookie visitas-->
+		<?php if(isset($_SESSION['username']))
+	{
+	?><?php
+				if (isset($_COOKIE['contador'])){
+					setcookie('contador', $_COOKIE['contador']+1);
+					echo "Número de visitas: ".$_COOKIE['contador'];		
+				} else {
+					setcookie('contador', 1);
+					echo "Bienvenido/a por primera vez a mi página web";
+					
+				}
+			?><?php } ?>
+
+			 
+	</head>
+    <body>
+		<div id="cabecera"><h1>FreeMusicStyle ♪♫♪♫</h1></div>
+			
+		<div id="menu">
+			<p><a href="html/Generos.html"><b><big><big><big><big>Géneros</b></big></big></big></big></a></p>
+			<p><a href="html/Artistas.html"><b><big><big><big><big>Artistas</b></big></big></big></big></a></p>
+			<p><a href="html/Contactar.html"><b><big><big><big><big>Contactar</b></big></big></big></big></a></p>
+				
+				<!--reloj / PHP-->
+		<iframe src="reloj/reloj.html" width="170px" height="50px" scrolling="no" frameborder="none" style="margin-top:30px;"></iframe>
+		
+
+				<!--Login-->
+		<a href="login.php" target="_self" style="margin-top:30px;"> <input type="button" name="Login" value="Login" id="boton3" /> </a></p>
+		<a href="registration.php" target="_self"> <input type="button" name="Registrar" value="Registrar" id="boton4" /> </a></p>
+		<a href="logout.php" target="_self"> <input type="button" name="Salir" value="Salir" id="boton5"/> </a></p>
+		
+				
+				<!--emisora radio-->
+		<a href="http://www.emisora.org.es/#radio-maria-espana" target="_blank" style="margin-top:50px;"><img title="emisora" alt="emisora" src="rec/radio.png"../rec/ .jpg"" width="200" height="100"></a> </p>
+		
+			<h3>Daniel Rodríguez & Kevin García</h3></p>
+	
+		</div>
+		
+		<div id="home" style="text-align: center; top: 50%; -ms-transform: translateY(-1%); -webkit-transform: translateY(-1%); transform: translateY(-1%);">
+				
+				<p><h2 style="color: black;"><big><big><big>Música Top 9</big></big></big></h2>
+					
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <big style="text-align:center;">Adrian Rodriguez & Juan Magan - Love Me Again</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/Adrian Rodriguez & Juan Magan - Love Me Again.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/Top/Adrian Rodriguez & Juan Magan - Love Me Again.mp3" download="Adrian Rodriguez & Juan Magan - Love Me Again">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+				
+					<big style="text-align:center;">Afrojack & David Guetta - Another Life</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/Afrojack & David Guetta - Another Life.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/Afrojack & David Guetta - Another Life.mp3" download="Afrojack & David Guetta - Another Life">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+					
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <big style="text-align:center;">Bad Bunny, J.Balvin, Ozuna & Arcangel - Soy Peor Remix</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/BAD BUNNY, J.BALVIN, OZUNA & ARCANGEL - SOY PEOR REMIX.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/BAD BUNNY, J.BALVIN, OZUNA & ARCANGEL - SOY PEOR REMIX.mp3" download="Bad Bunny, J.Balvin, Ozuna & Arcangel - Soy Peor Remix">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+					
+					<big style="text-align:center;">Cnco & Yande - Hey Dj</big>
+				<audio controls controlsList="nodownload" width="263" height="56"> 
+					<source src="rec/Top/CNCO & Yande - Hey Dj.mp3"  type="video/mp4"></source> 
+					</audio>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/CNCO & Yande - Hey Dj.mp3" download="Cnco & Yande - Hey Dj">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+									
+					<big style="text-align:center;">David Guetta & Justin Bieber - 2U</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/David Guetta & Justin Bieber - 2U.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/David Guetta & Justin Bieber - 2U.mp3" download="David Guetta & Justin Bieber - 2U">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+								
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <big style="text-align:center;">J.Balvin, Nicky Jam & Cosculluela - Ayer 2</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/J.Balvin, Nicky Jam & Cosculluela - Ayer 2.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/J.Balvin, Nicky Jam & Cosculluela - Ayer 2.mp3" download="J.Balvin, Nicky Jam & Cosculluela - Ayer 2">Descargar</a><?php } ?>
+	<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+					
+					&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp   <big style="text-align:center;">Luis Fonsi, Daddy Yankee & Justin Bieber - Despacito</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/Luis Fonsi, Daddy Yankee & Justin Bieber - Despacito.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/Luis Fonsi, Daddy Yankee & Justin Bieber - Despacito.mp3" download="Luis Fonsi, Daddy Yankee & Justin Bieber - Despacito">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p>
+								
+					<big style="text-align:center;">Ozuna & Wisin - Escápate Conmigo</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/Ozuna & Wisin - Escápate Conmigo.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/Ozuna & Wisin - Escápate Conmigo.mp3" download="Ozuna & Wisin - Escápate Conmigo">Descargar</a><?php } ?>
+									<form>
+										  <class="clasificacion">
+											&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label>
+									</form>
+									</p> 
+					
+					<big style="text-align:center;">Taburete - Sirenas</big>
+				<audio controls controlsList="nodownload" width="263" height="56">
+					<source src="rec/Top/Taburete - Sirenas.mp3"  type="video/mp4"></source>
+					</audio>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <?php if(isset($_SESSION['username']))
+	{
+	?><a href="rec/Taburete - Sirenas.mp3" download="Taburete - Sirenas">Descargar</a><?php } ?>
+								    <form>
+										  <class="clasificacion">
+											<input id="radio1" type="radio" name="estrellas" value="5"><!--
+											--><label for="radio1">★</label><!--
+											--><input id="radio2" type="radio" name="estrellas" value="4"><!--
+											--><label for="radio2">★</label><!--
+											--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+											--><label for="radio3">★</label><!--
+											--><input id="radio4" type="radio" name="estrellas" value="2"><!--
+											--><label for="radio4">★</label><!--
+											--><input id="radio5" type="radio" name="estrellas" value="1"><!--
+											--><label for="radio5">★</label> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+									</form>		
+			</div>	
+	</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registro</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+	require('db.php');
+    
+    if (isset($_REQUEST['username'])){
+		$username = stripslashes($_REQUEST['username']);
+		$username = mysqli_real_escape_string($con,$username); 
+		$email = stripslashes($_REQUEST['email']);
+		$email = mysqli_real_escape_string($con,$email);
+		$password = stripslashes($_REQUEST['password']);
+		$password = mysqli_real_escape_string($con,$password);
+
+		$trn_date = date("Y-m-d H:i:s");
+        $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'><h3>Registro satisfactorio.</h3><br/>Pinche aquí para iniciar sesión <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registro</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+
+</div>
+<?php } ?>
+
+<a href="index.php"><b><big>Atrás</big></b></a></p>
+
+</body>
+</html>
